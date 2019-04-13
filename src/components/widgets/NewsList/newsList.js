75e253181e914326a54f824ff733aeeb95b00618
Connor Payne <dev.cpayne@gmail.com>
2019-04-12 20:56:09 -0700

    copied tutorial

--- src/components/widgets/NewsList/newsList.js
+++ src/components/widgets/NewsList/newsList.js
@@ -75,6 +75,33 @@ loadMore = () => {
         )
       });
       break;
+      case('cardMain'):
+          template = this.state.items.map((item,i) => (
+              <CSSTransition
+              classNames={{
+                  enter:'newsList_wrapper',
+                  enterActive:'newsList_wrapper_enter'
+              }}
+              timeout={500}
+              key={i}
+              >
+                  <Link to={`/articles/${item.id}`}>
+                      <div className='flex_wrapper'>
+                          <div className='leftSideMain'
+                              style={{
+                                  background:`url('/images/articles/${item.image}')`
+                              }}>
+                              <div></div>
+                          </div>
+                          <div className='right'>
+                              <CardInfo teams={this.state.teams} team={item.team} date={item.date}/>
+                              <h2>{item.title}</h2>
+                          </div>
+                      </div>
+                  </Link>
+              </CSSTransition>
+          ))
+          break;
     default:
       template = null;
     }
--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,29 @@
-import React from 'react'
-// import PropTypes from 'prop-types'
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
-import NewsArticles from './components/Articles/News/Post/index';
-import VideoArticles from './components/Articles/Videos/Video/index';
 
+import NewsArticle from './components/Articles/News/Post/index';
+import VideoArticle from './components/Articles/Videos/Video/index'
+import NewsMain from './components/Articles/News/Main/index';
+import VideosMain from './components/Articles/Videos/Main/index';
 
+class Routes extends Component {
+    render(){
+        return(
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/news" exact component={NewsMain}/>
+                    <Route path="/articles/:id" exact component={NewsArticle}/>
+                    <Route path="/videos/:id" exact component={VideoArticle}/>
+                    <Route path="/videos" exact component={VideosMain}/>
+                </Switch>
+            </Layout>
 
-class Routes extends React.Component {
-  render () {
-    return(
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/articles/:id" exact component={NewsArticles}/>
-          <Route path="/videos/:id" exact component={VideoArticles}/>
-        </Switch>
-      </Layout>
-
-    )
-  }
+        )
+    }
 }
 
 export default Routes;
