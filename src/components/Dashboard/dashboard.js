c6861005cdbe17a5f6818950adc4262e857be7ab
Connor Payne <dev.cpayne@gmail.com>
2019-04-16 01:49:14 -0700

    text-editor created on dashboard

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,15 +1,159 @@
-import React from 'react'
+import React from 'react';
+import FormField from '../widgets/FormFields/formFields';
+import './dashboard.css';
 
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
+import { stateToHTML } from 'draft-js-export-html';
 
 class Dashboard extends React.Component {
 
   state = {
+    editorState: EditorState.createEmpty(),
+    postError:'',
+    loading:false,
+    formdata:{
+      author:{
+        element:'input',
+        value:'',
+        config:{
+          name:'author_input',
+          type:'text',
+          placeholder:'Enter your name'
+        },
+        validation:{
+          required:true,
+        },
+        valid:false,
+        touched:false,
+        validationMessage:''
+      },
+      title:{
+        element:'input',
+        value:'',
+        config:{
+          name:'title_input',
+          type:'text',
+          placeholder:'Enter the title'
+        },
+        validation:{
+          required:true,
+        },
+        valid:false,
+        touched:false,
+        validationMessage:''
+      },
+    }
+  }
+
+
+  updateForm = (element) => {
+    const newFormdata = {
+      ...this.state.formdata
+    }
+    const newElement = {
+      ...newFormdata[element.id]
+    }
+    newElement.value = element.event.target.value;
+
+    if(element.blur){
+      let validData = this.validate(newElement);
+      newElement.valid = validData[0];
+      newElement.validationMessage = validData[1];
+    }
+    newElement.touched = element.blur;
+    newFormdata[element.id] = newElement;
+
+    this.setState({
+      formdata:newFormdata
+    })
 
   }
 
-  submitForm = () => {
+
+  validate = (element) => {
+    let error = [true,''];
+
+    if(element.validation.email){
+      const valid = /\S+@\S+\.\S+/.test(element.value);
+      const message = `${!valid ? 'Must be a valid email':''}`;
+      error = !valid ? [valid,message] : error;
+    }
+
+    if(element.validation.password){
+      const valid = element.value.length >= 5;
+      const message = `${!valid ? 'Must be greater than 5 characters':''}`;
+      error = !valid ? [valid,message] : error;
+    }
+
+
+    if(element.validation.required){
+      const valid = element.value.trim() !=='';
+      const message = `${!valid ? 'This field is required':''}`;
+      error = !valid ? [valid,message] : error;
+    }
+
+    return error;
+  }
+
+
+
+  submitForm = (event) => {
+    event.preventDefault();
+
+    let dataToSubmit = {};
+    let formIsValid = true;
+
+    for(let key in this.state.formdata){
+      dataToSubmit[key] = this.state.formdata[key].value
+    }
+    for(let key in this.state.formdata){
+      formIsValid = this.state.formdata[key].valid && formIsValid;
+    }
+
+    console.log(dataToSubmit);
+
+    if(formIsValid){
+      console.log('SUBMIT POST')
+    }else{
+      this.setState({
+        postError:'Something went wrong'
+      })
+    }
 
   }
+
+
+  submitButton = () => (
+    this.state.loading ?
+      'loading...'
+      :
+      <div>
+        <button type='submit'>Add Post</button>
+      </div>
+  )
+
+
+  showError = () => (
+    this.state.postError !== '' ?
+    <div className='error'>{this.state.postError}</div>
+    : ''
+  )
+
+  onEditorStateChange = (editorState) =>{
+
+    let contentState = editorState.getCurrentContent();
+    let rawState = convertToRaw(contentState);
+
+    let html = stateToHTML(contentState)
+
+    console.log(html)
+
+    this.setState({
+      editorState
+    })
+  }
+
 
   render () {
     return(
@@ -17,8 +161,27 @@ class Dashboard extends React.Component {
         <form onSubmit={this.submitForm}>
           <h2>Add Post</h2>
 
-          dashboard
+            <FormField
+              id={'author'}
+              formdata={this.state.formdata.author}
+              change={(element)=>this.updateForm(element)}
+            />
 
+            <FormField
+              id={'title'}
+              formdata={this.state.formdata.title}
+              change={(element)=>this.updateForm(element)}
+            />
+
+          <Editor
+            editorState={this.state.editorState}
+            wrapperClassName='myEditor-wrapper'
+            editorClassName='myEditor-editor'
+            onEditorStateChange={this.onEditorStateChange}
+          />
+
+          {this.submitButton()}
+          {this.showError()}
         </form>
       </div>
     )
